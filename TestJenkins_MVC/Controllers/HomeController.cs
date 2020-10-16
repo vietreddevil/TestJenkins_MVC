c74120f5963a68cc0247aa4bda1aae0fa21d2b63
izuna <reddevil241097@gmail.com>
2020-10-16 11:12:50 +0700

    d

--- TestJenkins_MVC/Controllers/HomeController.cs
+++ TestJenkins_MVC/Controllers/HomeController.cs
@@ -9,8 +9,6 @@ using TestJenkins_MVC.Models;
 
 namespace TestJenkins_MVC.Controllers
 {
-    public class HomeController : Controller
-    {
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
